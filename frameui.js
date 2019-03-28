f558e7671db61feefea6882de8f605607df41729
ShenVi <yvban@outlook.com>
2019-03-28 18:03:38 +0800

    提交

--- frameui.js
+++ frameui.js
@@ -1,9 +1,9 @@
 //作者 ShenVi
 
-$(document).ready(function () {
- 
-    $('.frameui-header').load('common/header.html');
+        $(document).ready(function () {
 
-    $('.frameui-footer').load('common/footer.html');
+            $('.frameui-header').load('common-header.html');
 
-});
+            $('.frameui-footer').load('common-footer.html');
+
+        });
